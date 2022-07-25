d277fb08eb6a4cc50ffe871ee862b76b8d5d6850
Divar <z.darabi81@gmail.com>
2022-07-25 14:49:07 +0430

    http server

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-# This is a sample Python script.
+# Python 3 server example
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import os
+import time
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+hostName = "localhost"
+serverPort = 8080
 
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        time.sleep(10)
+        folder = "." + self.path
+        list_of_dir = [name for name in os.listdir(folder) if os.path.isdir(os.path.join(folder, name))]
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<html>\n<head>\n<title>dish dish</title>\n</head>\n", "utf-8"))
+        self.wfile.write(bytes("<body>\n", "utf-8"))
+        for name in list_of_dir:
+            self.wfile.write(bytes(f'<a href="{name}/">{name}<br></a>\n', "utf-8"))
+        self.wfile.write(bytes("</body>\n</html>\n", "utf-8"))
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
 
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    webServer.server_close()
+    print("Server stopped.")
